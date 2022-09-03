54b43c592d5bade4536eccc045837374ad8c4bee
Vit Listik <tivvit@seznam.cz>
2022-09-03 03:15:49 +0200

    timer

--- az.js
+++ az.js
@@ -1,8 +1,36 @@
+var time = 60;
+var intervalId;
+
 function question() {
     alert("a");
 }
 
-var hexes = document.querySelectorAll(".hex .content");
+function getQuestion() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "http://localhost:8080/question", false);
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.responseText);
+}
+
+function timer(time) {
+    if (intervalId !== null) {
+        clearInterval(intervalId);
+    }
+    var timer = document.querySelector("#timer");
+    timer.style.color = "black";
+    timer.textContent = time + "s"
+    intervalId = setInterval(() => {
+        time--;
+        timer.textContent = time + "s"
+        if (time == 0) {
+            clearInterval(intervalId);
+            timer.style.color = "red";
+            return false;
+        }
+    }, 1000)
+}
+
+var hexes = document.querySelectorAll(".hex");
 var questionBox = document.querySelector("#question");
 
 questionBox.querySelector("#close").addEventListener('click', () => {
@@ -13,23 +41,28 @@ questionBox.querySelector("#close").addEventListener('click', () => {
 
 hexes.forEach(hex => {
     hex.addEventListener('click', hex => {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", "http://localhost:8080/question", false);
-        xmlHttp.send( null );
-        var questionObject = JSON.parse(xmlHttp.responseText);
+        let hx = hex.target;
+        while (!hx.classList.contains("hex")) {
+           hx = hx.parentNode;
+        }
+        timer(time);
+
+        var questionObject = getQuestion();
 
         question = questionObject["question"];
         answer = questionObject["answer"];
 
-        var hx = hex.target.parentElement;
         questionBox.style.display = "block";
-        questionBox.querySelector("#number").innerText = hx.querySelector(".content").textContent;
+        questionBox.querySelector("#number").innerText = hx.textContent;
 
         questionBox.querySelector("#question-text").innerText = question;
         questionBox.querySelector("#answer").innerText = answer;
 
         questionBox.querySelector("#answer-button").onclick = () => {
             questionBox.querySelector("#answer").style.display = "block";
+        };
+        questionBox.querySelector("#time").onclick = () => {
+            timer(time);
         };
         questionBox.querySelector("#blue").onclick = () => {
             hx.className = "hex blue";
